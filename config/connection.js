bbda86f036a9282218eac330265fd52de1f48015
rubeenapk <rubeenaanwar5354@gmail.com>
2022-11-22 18:03:20 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,9 @@ const state={
 }
 
 module.exports.connect=(done)=>{
-    const url='mongodb://localhost:27017'
-    const dbname='shopping2'
+    //const url='mongodb://localhost:27017'
+    const url='mongodb+srv://rubeena:rubeenaanwar@cluster0.oegojvo.mongodb.net/shopping?retryWrites=true&w=majority'
+    const dbname='shopping'
 
 
     mongoClient.connect(url,(err,data)=>{
